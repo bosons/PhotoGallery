cf359e49de231c3c835b7230c09e87c249c523dd
bosons <xiwangy@gmail.com>
2015-05-10 22:24:30 -0700

    init commit

--- app/src/main/java/com/etuijian/android/photogallery2/PhotoGalleryFragment.java
+++ app/src/main/java/com/etuijian/android/photogallery2/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.etuijian.android.photogallery2;
 
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -54,13 +56,13 @@ public class PhotoGalleryFragment extends Fragment {
 
 
     private class PhotoHolder extends RecyclerView.ViewHolder{
-        private TextView mTitleTextView;
+        private ImageView mItemImageView;
         public PhotoHolder(View itemView) {
             super(itemView);
-            mTitleTextView =(TextView)itemView;
+            mItemImageView = (ImageView)itemView.findViewById(R.id.fragment_photo_gallery_image_view);
         }
-        public void bindGalleryItem(GalleryItem item) {
-            mTitleTextView.setText(item.toString());
+        public void bindDrawable(Drawable drawable) {
+            mItemImageView.setImageDrawable(drawable);
         }
     }
 
@@ -73,13 +75,15 @@ public class PhotoGalleryFragment extends Fragment {
 
         @Override
         public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            TextView textView = new TextView(getActivity());
-            return new PhotoHolder(textView);
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(R.layout.gallery_item, viewGroup, false);
+            return new PhotoHolder(view);
         }
 
         @Override
         public void onBindViewHolder(PhotoHolder photoHolder, int i) {
-            photoHolder.bindGalleryItem(mGalleryItemList.get(i));
+            Drawable placeHolder = getResources().getDrawable(R.drawable.abc_btn_radio_material);
+            photoHolder.bindDrawable(placeHolder);
         }
 
         @Override
